dc04882219f337c6bbbdb5d33a5faf618139d10e
shimingliangdm <shimingliang@deepmirror.com>
2022-01-09 00:09:04 +0800

    temp

--- app/src/main/java/com/example/socialapp/MainActivity.java
+++ app/src/main/java/com/example/socialapp/MainActivity.java
@@ -3,7 +3,16 @@ package com.example.socialapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.Manifest;
 import android.content.Context;
@@ -59,10 +68,30 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        LinearLayout lm = findViewById(R.id.linearLayout);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        /*for (int i=0; i<=4; i++)
+        {
+            TextView product = new TextView(this);
+            product.setText("testtest");
+            lm.addView(product);
+        }*/
+
+        for (int i=0; i<10; i++)
+        {
+            View testLayoutView = View.inflate(this, R.layout.test_layout, null);
+            lm.addView(testLayoutView);
+        }
+
+
 
         Thread thread = new Thread(new Runnable() {
 
@@ -70,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 try  {
                     //Your code goes here
-                    String returnString = connectTomcatServer("http://192.168.136.244:8080/socialappservice/servlet/HelloServlet/");
+                    String returnString = connectTomcatServer("http://192.168.101.17:8080/socialappservice/servlet/HelloServlet/");
                     Log.i(getClass().getSimpleName(), "received message is: " + returnString);
                 } catch (Exception e) {
                     e.printStackTrace();
